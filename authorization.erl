a4894433bb89b9357dff66b01871d084bedf0f33
Alysson de Sousa Ribeiro <alyssondsr@hithun.com>
2016-11-28 21:13:28 -0200

    ajustes

--- authorization.erl
+++ authorization.erl
@@ -13,12 +13,14 @@ execute(Request) ->
 	ResponseType = ems_request:get_querystring(<<"response_type">>, "", Request),
 	RedirectUri = ems_request:get_querystring(<<"redirect_uri">>, "", Request),
 	ClientId = ems_request:get_querystring(<<"client_id">>, "", Request),
+	Secret = ems_request:get_querystring(<<"secret">>, "", Request),
 	State = ems_request:get_querystring(<<"state">>, "", Request),
 	Scope = ems_request:get_querystring(<<"scope">>, "", Request),
-	%Resposta = oauth2:authorize_code_request(ClientId, RedirectUri, "", "", Scope, ""),
 
-	io:format("\nresponse_type = ~p\nclient_id = ~p \nredirect_uri = ~p \nstate = ~p \nscope = ~p\n_____________\n", [ResponseType, ClientId, RedirectUri, State, Scope]),
-	
+	io:format("\nresponse_type = ~p\nclient_id = ~p \nredirect_uri = ~p \nstate = ~p \nSecret = ~p\n_____________\n", [ResponseType, ClientId, RedirectUri, State, Secret]),
+	Resposta = ems_oauth2_backend:authenticate_client(ClientId,Secret,""),
+	Unio:format("\nAutenticação = ~p\n_____________\n", [Resposta]),
+
 	%io:format("Requisição ~p",[Request]),
 	<<"{\"ok\": \"ok\"}">>.
 	
