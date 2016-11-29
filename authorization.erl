88acfcab5a5fa20447a3c0ee87b13752e4a5bffb
Alysson de Sousa Ribeiro <alyssondsr@hithun.com>
2016-11-29 17:52:14 -0200

    teste

--- authorization.erl
+++ authorization.erl
@@ -20,8 +20,31 @@ execute(Request) ->
 	io:format("\nresponse_type = ~p\nclient_id = ~p \nredirect_uri = ~p \nstate = ~p \nSecret = ~p\n_____________\n", [ResponseType, ClientId, RedirectUri, State, Secret]),
 	Resposta = ems_oauth2_backend:authenticate_client(ClientId,Secret,""),
 	io:format("\nAutenticação = ~p\n_____________\n", [Resposta]),
+	process_password_grant(ClientId, Secret, Scope, Request),
 
 	%io:format("Requisição ~p",[Request]),
 	<<"{\"ok\": \"ok\"}">>.
 	
 
+process_password_grant(ClientId, Secret, Scope, Request) ->
+	%io:format(".............\n Username: ~p \n Passwd: ~p \n Scope: ~p \n...............", [Username,Password,Scope] ),
+	Auth = oauth2:authorize_password(ClientId, Secret, Scope, []),
+	%io:format(".............\n Auth: ~p \n...............", [Auth] ),
+	issue_token(Auth, Request).
+	
+issue_token({ok, Auth}, Request) ->
+    emit_response(oauth2:issue_token(Auth, []), Request);
+issue_token(Error, Request) ->
+    emit_response(Error, Request).
+
+emit_response(AuthResult, Req) ->
+    {Code, JSON} =
+        case AuthResult of
+            {error, Reason} ->
+                {400, jsx:encode([{error, to_binary(Reason)}])};
+            Response ->
+                {200, jsx:encode(to_json_term(oauth2_response:to_proplist(Response), []))}
+        end,
+    cowboy_req:reply(Code, [], JSON, Req).
+to_binary(Atom) when is_atom(Atom) ->
+    list_to_binary(atom_to_list(Atom)).
