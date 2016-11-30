69eb70bedfe9804208d94306c579e368e1d3e737
Alysson de Sousa Ribeiro <alyssondsr@hithun.com>
2016-11-30 15:53:33 -0200

    Client credencials

--- authorization.erl
+++ authorization.erl
@@ -8,6 +8,8 @@
 -module(authorization).
 
 -export([execute/1]).
+-export([issue_token/2]).
+
 
 execute(Request) -> 
 	ResponseType = ems_request:get_querystring(<<"response_type">>, "", Request),
@@ -17,23 +19,28 @@ execute(Request) ->
 	State = ems_request:get_querystring(<<"state">>, "", Request),
 	Scope = ems_request:get_querystring(<<"scope">>, "", Request),
 
-	io:format("\nresponse_type = ~p\nclient_id = ~p \nredirect_uri = ~p \nstate = ~p \nSecret = ~p\n_____________\n", [ResponseType, ClientId, RedirectUri, State, Secret]),
-	Resposta = ems_oauth2_backend:authenticate_client(ClientId,Secret,""),
-	io:format("\nAutenticação = ~p\n_____________\n", [Resposta]),
-	process_password_grant(ClientId, Secret, Scope, Request),
+	%io:format("\nresponse_type = ~p\nclient_id = ~p \nredirect_uri = ~p \nstate = ~p \nSecret = ~p\n_____________\n", [ResponseType, ClientId, RedirectUri, State, Secret]),
+	
+	Resposta = process_client_credentials_grant(ClientId, Secret, Scope, Request),
+	%io:format("\nAutenticação = ~p\n_____________\n", [Resposta2]),
 
 	%io:format("Requisição ~p",[Request]),
-	<<"{\"ok\": \"ok\"}">>.
+	Resposta.
 	
-
+process_client_credentials_grant(ClientId, Secret, Scope, Request) ->
+    Auth = oauth2:authorize_client_credentials(ClientId, Secret, Scope, []),
+	issue_token(Auth, Request).
+    
 process_password_grant(ClientId, Secret, Scope, Request) ->
-	%io:format(".............\n Username: ~p \n Passwd: ~p \n Scope: ~p \n...............", [Username,Password,Scope] ),
+	%io:format(".............\n Username: ~p \n Passwd: ~p \n Scope: ~p \n...............\n", [Username,Password,Scope] ),
 	Auth = oauth2:authorize_password(ClientId, Secret, Scope, []),
-	%io:format(".............\n Auth: ~p \n...............", [Auth] ),
+	%io:format("..............\n Auth: ~p \n...............", [Auth] ),
 	issue_token(Auth, Request).
 	
 issue_token({ok, Auth}, Request) ->
-    emit_response(oauth2:issue_token(Auth, []), Request);
+	Response = oauth2:issue_token(Auth, []),
+	%io:format("\n#################\nToken = ~p\n#################\n", [Response]),
+    oauth2_response:to_proplist(Response);
 issue_token(Error, Request) ->
     emit_response(Error, Request).
 
@@ -48,3 +55,11 @@ emit_response(AuthResult, Req) ->
     cowboy_req:reply(Code, [], JSON, Req).
 to_binary(Atom) when is_atom(Atom) ->
     list_to_binary(atom_to_list(Atom)).
+    
+to_json_term([], Acc) ->
+    Acc;
+to_json_term([{H, {HK, HV}} |  T], Acc) ->
+    to_json_term(T, [{H, <<"{",HK/binary,",",HV/binary,"}">>} | Acc]);
+to_json_term([H | T], Acc) ->
+    to_json_term(T, [H | Acc]).
+
